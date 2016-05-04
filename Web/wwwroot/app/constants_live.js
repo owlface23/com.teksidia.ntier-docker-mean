085effd9af9e7fb0de012467c084940582cf342b
Jamie Cox <jamie.cox@stockport.gov.uk>
2016-05-04 14:08:47 +0100

    Changed api url

--- Web/wwwroot/app/constants_live.js
+++ Web/wwwroot/app/constants_live.js
@@ -1,5 +1,7 @@
 ﻿
 angular.module('testApp')
   .constant("URLS", {
-    "serviceApi": "http://192.168.99.100:8888/api/ServiceRequest"
+    //"serviceApi": "http://192.168.99.100:8888/api/ServiceRequest" // local docker
+    //"serviceApi": "http://localhost:8888/api/ServiceRequest" // locally via gulp
+    "serviceApi": "http://13.95.145.127:8888/api/ServiceRequest" // azure vm
   });
